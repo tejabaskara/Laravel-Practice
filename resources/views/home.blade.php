0b7c64c12fe3f241bcf45407185b37c915a09865
niceguy <tejasidang@gmail.com>
2025-03-14 13:39:34 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>try</title>
+</head>
+<body>
+    <h1>Hello all</h1>
+    
+</body>
+</html>
